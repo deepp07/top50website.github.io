3e68044fb7be01de617a8097cac21388cf522c79
Deep Patel <89537960+deepp07@users.noreply.github.com>
2024-04-26 14:19:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     // Social Media Sharing Functions 
     function shareOnFacebook() { 
         // Replace 'your-website-url' with the URL you want to share 
-        const urlToShare = 'https://www.example.com'; 
+        const urlToShare = 'https://deepp07.github.io/top50website.github.io/index.html'; 
         const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`; 
         window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400'); 
     } 
    
     function shareOnTwitter() { 
         // Replace 'your-website-url' and 'your-text' with your desired URL and text 
-        const urlToShare = 'your-website-url'; 
+        const urlToShare = 'https://deepp07.github.io/top50website.github.io/index.html'; 
         const textToShare = 'your-text'; 
         const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`; 
         window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400'); 
